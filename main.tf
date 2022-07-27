de0e790724df282247ca80fc116d51b200ebcc67
Jonatan Sombié <jonatan.sombie@gmail.com>
2022-07-27 10:01:03 +0200

    clean up required providers

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
 terraform {
   backend "s3" {}
-
-  required_providers {
-
-    auth0 = {
-      source  = "registry.terraform.io/auth0/auth0"
-      version = "0.32.0"
-    }
-  }
 }
 
 output "DREAM_CLOUD_API_AUDIENCE" {
