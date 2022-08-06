745a0441ce9f48d14ec40944dd4d41c641a6358a
Jonatan Sombié <jonatan.sombie@gmail.com>
2022-08-06 18:04:01 +0200

    update project id

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
 
 output "DREAM_CLOUD_API_AUDIENCE" {
   sensitive = true
-  value     = "https://dream-cloud-api-poc.${var.dream_workspace}"
+  value     = "https://62e95a35ad5d2ac51db55220.${var.dream_workspace}"
 }
 
 output "DREAM_CLOUD_API_URL" {
